b5b8673c59940f993871796e84116f977d1e7ae7
Nitish Khagwal <nitishkmrk@gmail.com>
2018-12-01 17:18:07 +0530

    v1.0.3 - Added UI Messages

--- src/script.js
+++ src/script.js
@@ -2,12 +2,17 @@ import sketch from 'sketch'
 import UI from 'sketch/ui'
 
 export default function () {
-  var string = UI.getStringFromUser("Enter New Layer Name", 'unnamed')
+  var userInput = UI.getStringFromUser("Enter New Layer Name", 'unnamed')
   const doc = sketch.getSelectedDocument()
   const selectedLayers = doc.selectedLayers
-  selectedLayers.forEach(
-    layer => {
-      layer.name = string
-    }
-  )
+  if (selectedLayers.length != 0) {
+    selectedLayers.forEach(
+      layer => {
+        layer.name = userInput
+      }
+    )
+    UI.message(`${selectedLayers.length} selection(s) have been renamed 👍`)
+  } else {
+    UI.message(`🛑 Please select at least one layer/group/artboard to rename it`)
+  }
 }
