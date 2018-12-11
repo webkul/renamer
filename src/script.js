bf2022f2e86816322dd9a465da4d990393097a02
Nitish Khagwal <nitishkmrk@gmail.com>
2018-12-11 23:48:21 +0530

    Fixed Reported Issue

--- src/script.js
+++ src/script.js
@@ -5,21 +5,17 @@ export default function () {
   var userInput = UI.getStringFromUser("Enter New Layer Name", 'unnamed')
   const doc = sketch.getSelectedDocument()
   const selectedLayers = doc.selectedLayers
-  if (selectedLayers.length != 0 && selectedLayers.length != null) {
-    selectedLayers.forEach(
-      layer => {
-        layer.name = userInput
-      }
-    )
-    UI.message(`${selectedLayers.length} selection(s) have been renamed 👍`)
-  } else if (selectedLayers.length != 0 && selectedLayers.length == null) {
-    selectedLayers.forEach(
-      layer => {
-        layer.name = userInput
-      }
-    )
-    UI.message(`${selectedLayers.length} selection(s) have been renamed 👍`)
-  } else {
-    UI.message(`🛑 Please select at least one layer/group/artboard to rename it`)
+
+  if (userInput !== "null") {
+    if (selectedLayers.length != 0) {
+      selectedLayers.forEach(
+        layer => {
+          layer.name = userInput
+        }
+      )
+      UI.message(`${selectedLayers.length} selection(s) have been renamed 👍`)
+    } else {
+      UI.message(`🛑 Please select at least one layer/group/artboard to rename it`)
+    }
   }
 }
